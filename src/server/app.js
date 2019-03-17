8bdc9c6c05cf061dbb795cea1c227e46ce0701c4
Harrm <harrm@mail.ru>
2019-03-17 21:31:02 +0300

    Alpha version
    Co-authored-by: iambackend <iambackend@gmail.com>

--- src/server/app.js
+++ src/server/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 let app = express();
 app.use(express.static('public'));
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html')});
+
 const port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Listening on port ' + port);
